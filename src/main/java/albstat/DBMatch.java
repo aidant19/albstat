f12d4ac66cfb23b52514c36bb4e99b2d461cf314
Aidan Tokarski <aidant19@gmail.com>
2020-06-14 20:41:15 -0400

    (Continued file merging) reformated DBMatch

--- src/main/java/albstat/DBMatch.java
+++ src/main/java/albstat/DBMatch.java
@@ -6,7 +6,7 @@ package albstat;
 
 import java.util.ArrayList;
 
-public class DBMatch{
+public class DBMatch {
 
     public String match_id;
     public char match_level;
@@ -16,7 +16,7 @@ public class DBMatch{
     public ArrayList<DBMatchPlayer> dbMatchPlayers;
     public ArrayList<DBSnapshot> dbSnapshots;
 
-    public DBMatch(Match match, int starting_id){
+    public DBMatch(Match match, int starting_id) {
         this.match_id = match.matchID;
         this.match_level = (char) match.level;
         this.match_winner = (char) match.winner;
@@ -26,42 +26,47 @@ public class DBMatch{
         this.dbSnapshots = getDBSnapshots(match);
     }
 
-    public ArrayList<DBMatchPlayer> getDBMatchPlayers(Match match, int starting_id){
+    public ArrayList<DBMatchPlayer> getDBMatchPlayers(Match match, int starting_id) {
         ArrayList<DBMatchPlayer> players = new ArrayList<>();
         for (String player : match.team1Players) {
-            players.add(new DBMatchPlayer(starting_id++, player, match.matchID, 1));            
+            players.add(new DBMatchPlayer(starting_id++, player, match.matchID, 1));
         }
         for (String player : match.team2Players) {
-            players.add(new DBMatchPlayer(starting_id++, player, match.matchID, 2));            
+            players.add(new DBMatchPlayer(starting_id++, player, match.matchID, 2));
         }
         return players;
     }
 
-    public ArrayList<DBSnapshot> getDBSnapshots(Match match){
+    public ArrayList<DBSnapshot> getDBSnapshots(Match match) {
         ArrayList<DBSnapshot> snapshots = new ArrayList<>();
         for (Event event : match.events) {
-            snapshots.add(new DBSnapshot(getMatchPlayerID(event.player1ID), 1, event.eventID, event.timestamp, event.player1Snapshot));
-            snapshots.add(new DBSnapshot(getMatchPlayerID(event.player2ID), 2, event.eventID, event.timestamp, event.player2Snapshot));
+            snapshots.add(new DBSnapshot(getMatchPlayerID(event.player1ID), 1, event.eventID, event.timestamp,
+                    event.player1Snapshot));
+            snapshots.add(new DBSnapshot(getMatchPlayerID(event.player2ID), 2, event.eventID, event.timestamp,
+                    event.player2Snapshot));
             for (MainHandSnapshot groupSnap : event.group.snapshots) {
-                snapshots.add(new DBSnapshot(getMatchPlayerID(groupSnap.playerID), 3, event.eventID, event.timestamp, groupSnap));
+                snapshots.add(new DBSnapshot(getMatchPlayerID(groupSnap.playerID), 3, event.eventID, event.timestamp,
+                        groupSnap));
             }
             for (ParticipantSnapshot partiSnap : event.participants.snapshots) {
-                snapshots.add(new DBSnapshot(getMatchPlayerID(partiSnap.playerID), 4, event.eventID, event.timestamp, partiSnap));
+                snapshots.add(new DBSnapshot(getMatchPlayerID(partiSnap.playerID), 4, event.eventID, event.timestamp,
+                        partiSnap));
             }
         }
         return snapshots;
     }
 
-    public int getMatchPlayerID(String player_id){
+    public int getMatchPlayerID(String player_id) {
         for (DBMatchPlayer dbMatchPlayer : dbMatchPlayers) {
-            if(dbMatchPlayer.player_id.compareTo(player_id) == 0){
+            if (dbMatchPlayer.player_id.compareTo(player_id) == 0) {
                 return dbMatchPlayer.match_player_id;
             }
         }
         return -1;
     }
 
-    public String toString(){
-        return String.format("('%s', '%d', '%d', '%s', '%s')", match_id, (int)match_level, (int)match_winner, match_start_time, match_end_time);
+    public String toString() {
+        return String.format("('%s', '%d', '%d', '%s', '%s')", match_id, (int) match_level, (int) match_winner,
+                match_start_time, match_end_time);
     }
 }
