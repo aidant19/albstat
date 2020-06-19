7504534b5122e4893f2d19e90762dba54f933128
Aidan Tokarski <aidant19@gmail.com>
2020-06-18 23:59:04 -0400

    Initial file pruning (deprecated code cleaning)

--- src/main/java/albstat/DataVerifier.java
+++ src/main/java/albstat/DataVerifier.java
@@ -8,31 +8,4 @@ import java.util.ArrayList;
 
 public class DataVerifier {
 
-    public static int verifyData(ArrayList<Event> events, MatchResult results) {
-        // verifies match data by comparing the match results retrieved from the api
-        // with results created from retrieved events
-        MatchResult resultsReported = new MatchResult(results.matchID);
-        resultsReported.setPlayers(getTeam1(results), getTeam2(results));
-        for (Event event : events) {
-            resultsReported.updateResult(event.player1ID, true);
-            resultsReported.updateResult(event.player2ID, false);
-        }
-        return results.compare(resultsReported);
-    }
-
-    public static ArrayList<String> getTeam1(MatchResult results) {
-        ArrayList<String> playerIDs = new ArrayList<String>();
-        for (PlayerResult result : results.team1Results) {
-            playerIDs.add(result.playerID);
-        }
-        return playerIDs;
-    }
-
-    public static ArrayList<String> getTeam2(MatchResult results) {
-        ArrayList<String> playerIDs = new ArrayList<String>();
-        for (PlayerResult result : results.team2Results) {
-            playerIDs.add(result.playerID);
-        }
-        return playerIDs;
-    }
 }
--- src/main/java/albstat/Driver.java
+++ src/main/java/albstat/Driver.java
@@ -1,88 +1,14 @@
 package albstat;
 
-import java.sql.SQLException;
-
 // aidan tokarski
 // 6/3/20
 // albstat driver module
 
-import java.util.ArrayList;
 
 public class Driver {
 
-    public static void getNewMatches(int limit, int offset, int total) throws Exception {
-        for (; limit + offset <= total; offset += limit) {
-            APIInterface apiInterface = new APIInterface();
-            DBInterface dbInterface = new DBInterface();
-            ArrayList<String> matchIDs = dbInterface.getParsedMatchIDs();
-            /* ArrayList<Match> matchList = apiInterface.getNewMatches(offset, limit, matchIDs);
-            try {
-                dbInterface.addMatches(matchList);
-                dbInterface.commitClose();
-            } catch (SQLException e) {
-                System.out.println(e);
-                dbInterface.rollbackClose();
-            } */
-        }
-    }
-
-    public static void getPlayerNames() throws Exception {
-        APIInterface apiInterface = new APIInterface();
-        DBInterface dbInterface = new DBInterface();
-        ArrayList<DBPlayer> players = dbInterface.getUnnamedPlayers();
-        int parsed = 0;
-        for (DBPlayer player : players) {
-            player.setName(apiInterface.getPlayerName(player.player_id));
-            parsed++;
-            apiInterface.reportStatus(String.format("progress: %d / %d (%.2f%%)", parsed, players.size(),
-                    100 * parsed / (double) players.size()), false, false);
-            try {
-                dbInterface.addPlayer(player);
-                dbInterface.commit();
-            } catch (SQLException e) {
-                System.out.println(e);
-                dbInterface.rollback();
-            }
-        }
-        apiInterface.reportStatus(String.format("progress: %d / %d (%.2f%%)", parsed, players.size(),
-                100 * parsed / (double) players.size()), false, true);
-    }
-
-    public static void getItemNames() throws Exception {
-
-        String[] item_classes = { "mainhand", "offhand", "head", "armor", "shoe", "cape" };
-        for (String item_class : item_classes) {
-            APIInterface apiInterface = new APIInterface();
-            DBInterface dbInterface = new DBInterface();
-            int parsed = 0;
-            ArrayList<DBItem> items = dbInterface.getUnnamedItems(item_class);
-            for (DBItem item : items) {
-                item.setName(apiInterface.getItemName(item.item_type));
-                parsed++;
-                apiInterface.reportStatus(String.format("progress: %d / %d (%.2f%%)", parsed, items.size(),
-                        100 * parsed / (double) items.size()), false, false);
-                try {
-                    dbInterface.addItem(item);
-                    dbInterface.commit();
-                } catch (SQLException e) {
-                    System.out.println(e);
-                    dbInterface.rollback();
-                }
-            }
-            apiInterface.reportStatus(String.format("progress: %d / %d (%.2f%%)", parsed, items.size(),
-                    100 * parsed / (double) items.size()), false, true);
-        }
-    }
-
-    public static void updateSnapshots() throws Exception {
-        DBInterface dbInterface = new DBInterface();
-        dbInterface.updateSnapshots();
-        dbInterface.commitClose();
-    }
-/* 
     public static void main(String[] args) throws Exception {
         DataBuilder builder = new DataBuilder();
         builder.getNewMatches(0, 1, 1);
     }
-*/
 }
