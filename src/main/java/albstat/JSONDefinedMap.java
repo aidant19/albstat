5c73726ac1649b3ecb5878b634345db943bf3ee1
aidant19 <aidant19@gmail.com>
2021-05-30 23:27:59 -0400

    Merged JSONMap with JSONDefinedMap (corrected)

--- src/main/java/albstat/JSONDefinedMap.java
+++ src/main/java/albstat/JSONDefinedMap.java
@@ -9,6 +9,7 @@ import java.util.AbstractSet;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.Iterator;
+import java.util.Map;
 
 public abstract class JSONDefinedMap extends AbstractMap<String, String> {
 
@@ -110,5 +111,61 @@ public abstract class JSONDefinedMap extends AbstractMap<String, String> {
         toString = toString.substring(0, toString.length() - 1);
         toString += ")";
         return toString;
+    }
+
+    public class JSONMap extends AbstractMap<String, String[]> {
+
+        private int size;
+        private int entryCount;
+        private String[] keyNames; // name of the key the address maps to
+        private String[][] Addresses; // JSON address
+    
+        public JSONMap(int size) {
+            this.size = size;
+            this.entryCount = 0;
+            this.Addresses = new String[size][];
+            this.keyNames = new String[size];
+        }
+    
+        public boolean add(String[] address, String keyName) {
+            Addresses[entryCount] = address;
+            keyNames[entryCount] = keyName;
+            entryCount++;
+            return true;
+        }
+    
+        public Set<Entry<String, String[]>> entrySet() {
+            return new EntrySet();
+        }
+    
+        final class EntrySet extends AbstractSet<Map.Entry<String, String[]>> {
+    
+            public final int size() {
+                return size;
+            }
+    
+            public Iterator<Entry<String, String[]>> iterator() {
+                return new EntryIterator();
+            }
+    
+            final class EntryIterator implements Iterator<Entry<String, String[]>> {
+    
+                private int index;
+    
+                EntryIterator() {
+                    this.index = 0;
+                }
+    
+                public final Entry<String, String[]> next() {
+                    Entry<String, String[]> nextEntry = new SimpleEntry<>(keyNames[index], Addresses[index]);
+                    index++;
+                    return nextEntry;
+                }
+    
+                public final boolean hasNext() {
+                    return index != entryCount;
+                }
+            }
+        }
     }
 }
