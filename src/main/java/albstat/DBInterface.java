ed44dc01a77d87b19756683ed33492445a73987f
aidant19 <aidant19@gmail.com>
2021-05-30 23:22:40 -0400

    Merged DBCredentials with DBInterface

--- src/main/java/albstat/DBInterface.java
+++ src/main/java/albstat/DBInterface.java
@@ -9,8 +9,15 @@ package albstat;
 // 6/29/20
 // + automatic snapshot weighing
 
+// util
 import java.sql.*;
 import java.util.ArrayList;
+
+// credential reading
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class DBInterface {
 
@@ -239,6 +246,39 @@ public class DBInterface {
         } catch (AssertionError | SQLException e) {
             e.printStackTrace();
             System.out.println(e);
+        }
+    }
+
+    private static class DBCredentials {
+
+        private String user;
+        private String pass;
+    
+        public DBCredentials(){
+            // this constructor assumes you have placed a cred.txt file in the proper location
+            readCred();
+        }
+    
+        private void readCred(){
+            try {
+                FileReader fileReader = new FileReader("cred.txt");
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                this.user = bufferedReader.readLine();
+                this.pass = bufferedReader.readLine();
+                bufferedReader.close();
+            } catch (FileNotFoundException ex) {
+                System.out.println("credential file not found");
+            } catch (IOException ex) {
+                System.out.println("error reading credentials");
+            }
+        }
+    
+        public String getUser(){
+            return this.user;
+        }
+    
+        public String getPass(){
+            return this.pass;
         }
     }
 }
